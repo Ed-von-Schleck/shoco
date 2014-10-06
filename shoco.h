0874d63d240d81850423bc04f1778ce0df9d79a1
Cameron <cameron@moodycamel.com>
2014-10-06 11:11:18 -0400

    Got shoco to compile without warnings under MSVC2013

--- shoco.h
+++ shoco.h
@@ -2,13 +2,21 @@
 
 #include <stddef.h>
 
+#if defined(_MSC_VER)
+#define shoco_restrict __restrict
+#else
+#define shoco_restrict restrict
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-size_t shoco_compress(const char * const in, size_t len, char * const out, size_t bufsize);
-size_t shoco_decompress(const char * const in, size_t len, char * const out, size_t bufsize);
+size_t shoco_compress(const char * const shoco_restrict in, size_t len, char * const shoco_restrict out, size_t bufsize);
+size_t shoco_decompress(const char * const shoco_restrict in, size_t len, char * const shoco_restrict out, size_t bufsize);
 
 #ifdef __cplusplus
 }
 #endif
+
+#undef shoco_restrict
