3bf175e71436d88f5a782646d68ffcb40eec154d
Ryan Rowe <mega.gamer05@gmail.com>
2016-01-21 22:48:49 -0600

    fix versions

--- setup.py
+++ setup.py
@@ -2,4 +2,4 @@ from distutils.core import setup, Extension
 
 pyshoco = Extension('pyshoco', sources = ['pyshoco.c', 'shoco.c'], include = ['shoco.h'], include_dirs = ['.'], extra_compile_args = ['-std=c99'])
 
-setup(name = 'pyshoco', version = '1.0', description = 'Shoco Python wrapper module', ext_modules = [pyshoco], author = 'R. Rowe', author_email = 'mega.gamer05@gmail.com', url = 'https://testpypi.python.org/pypi/pyshoco/1.0')
+setup(name = 'pyshoco', version = '1.1', description = 'Shoco Python wrapper module', ext_modules = [pyshoco], author = 'R. Rowe', author_email = 'mega.gamer05@gmail.com', url = 'https://testpypi.python.org/pypi/pyshoco/')
