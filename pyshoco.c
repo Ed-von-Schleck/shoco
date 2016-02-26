31a8f899111e29b7a9bac7d4323339e8781daff6
Ryan Rowe <Mego@users.noreply.github.com>
2016-02-26 02:37:01 -0600

    add py3k support

--- pyshoco.c
+++ pyshoco.c
@@ -2,6 +2,46 @@
 #include <string.h>
 #include "shoco.h"
 
+#if PY_MAJOR_VERSION >= 3
+#define IS_PY3K
+#endif
+
+static PyObject *pyshoco_compress(PyObject *self, PyObject *args);
+static PyObject *pyshoco_decompress(PyObject *self, PyObject *args);
+
+static PyMethodDef ShocoMethods[] = {
+    {"compress",  pyshoco_compress, METH_VARARGS,
+     "Compress a string."},
+    {"decompress",  pyshoco_decompress, METH_VARARGS,
+     "Decompress a string."},
+    {NULL, NULL, 0, NULL}        /* Sentinel */
+};
+
+#ifdef IS_PY3K
+
+static struct PyModuleDef shocomodule = {
+   PyModuleDef_HEAD_INIT,
+   "pyshoco",   /* name of module */
+   NULL, /* module documentation, may be NULL */
+   -1,       /* size of per-interpreter state of the module,
+                or -1 if the module keeps state in global variables. */
+   ShocoMethods
+};
+
+PyMODINIT_FUNC PyInit_pyshoco(void)
+{
+    return PyModule_Create(&shocomodule);
+}
+
+#else
+    
+PyMODINIT_FUNC initpyshoco(void)
+{
+    (void) Py_InitModule("pyshoco", ShocoMethods);
+}
+
+#endif
+
 static PyObject *pyshoco_compress(PyObject *self, PyObject *args) {
     const char *in;
 
@@ -26,17 +66,4 @@ static PyObject *pyshoco_decompress(PyObject *self, PyObject *args) {
     memset(out, 0, bufsize);
     shoco_decompress(in, strlen(in), out, bufsize);
     return Py_BuildValue("s", out);
-}
-
-static PyMethodDef ShocoMethods[] = {
-    {"compress",  pyshoco_compress, METH_VARARGS,
-     "Compress a string."},
-    {"decompress",  pyshoco_decompress, METH_VARARGS,
-     "Decompress a string."},
-    {NULL, NULL, 0, NULL}        /* Sentinel */
-};
-
-PyMODINIT_FUNC initpyshoco(void)
-{
-    (void) Py_InitModule("pyshoco", ShocoMethods);
 }
